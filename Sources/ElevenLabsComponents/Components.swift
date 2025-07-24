2a8e6c22386ccce074f4bd586eb5f01e4af04681
Louis Jordan <louisjoecodes@gmail.com>
2025-07-24 11:33:44 +0100

    chore: bump version

--- Sources/ElevenLabsComponents/Components.swift
+++ Sources/ElevenLabsComponents/Components.swift
@@ -18,7 +18,7 @@
 @_exported import LiveKit
 @_exported import SwiftUI
 
-public let elevenlabsComponentsVersion = "0.1.0"
+public let elevenlabsComponentsVersion = "0.1.1"
 
 public typealias ComponentBuilder<Content: View> = () -> Content
 public typealias ParticipantComponentBuilder<Content: View> = (_: Participant) -> Content
